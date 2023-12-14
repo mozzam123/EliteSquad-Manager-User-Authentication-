f92b4e5b75f35d898e3d15977da140b156d148df
mozzam123 <mozzam607@gmail.com>
2023-12-14 17:24:49 +0530

    added functionality to button

--- team-service/src/public/js/ownedPlayers.js
+++ team-service/src/public/js/ownedPlayers.js
@@ -1,3 +1,83 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.log('File is running*********************');
+
+//     function deleteTask(id) {
+//         // ... (your deleteTask function code)
+//         fetch(`http://127.0.0.1:2222/api/player/delete?id=${id}`, {
+//         method: 'DELETE',
+//     })
+//         .then(response => {
+//             if (response.ok) {
+//                 window.location.reload()
+//                 console.log('Deleted succesfull');
+//             } else {
+//                 console.error("Failed to delete a task")
+//             }
+//         }).catch(error => console.error('Error: ', error))
+//     }
+
+//     function openCreatePlayerModal() {
+//         $('#createPlayerModal').modal('show');
+//     }
+
+//     // Use querySelector instead of getElementById
+//     document.querySelector('#createPlayerForm').addEventListener('submit', function (event) {
+//         event.preventDefault();
+//         createPlayer();
+//     });
+
+    
+
+//     function createPlayer() {
+//         // Get form values
+//     var name = document.getElementById('name').value;
+//     var position = document.getElementById('position').value;
+//     var height = document.getElementById('height').value;
+//     var nationality = document.getElementById('nationality').value;
+//     var weight = document.getElementById('weight').value;
+
+//     // Create player object
+//     var newPlayer = {
+//         "name": name,
+//         "position": position,
+//         "height": height,
+//         "nationality": nationality,
+//         "weight": weight,
+//         "user": "6569631e738444d9987e170c"  // You may need to dynamically set the user ID
+//     };
+
+//     // Make AJAX request
+//     fetch('http://127.0.0.1:2222/api/player/create', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(newPlayer),
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             window.location.reload()
+//         })
+//         .then(data => {
+//             // Handle the response data as needed
+//             console.log(data);
+
+//             // Close the modal
+//             $('#createPlayerModal').modal('hide');
+//         })
+//         .catch(error => console.error('Error:', error));
+//     }
+// });
+
+
+
+
+
+
+
+console.log('File is running*********************');
 function deleteTask(id) {
     fetch(`http://127.0.0.1:2222/api/player/delete?id=${id}`, {
         method: 'DELETE',
@@ -12,3 +92,108 @@ function deleteTask(id) {
         }).catch(error => console.error('Error: ', error))
 
 }
+
+function openCreatePlayerModal() {
+    $('#createPlayerModal').modal('show');
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // Use querySelector instead of getElementById
+    document.querySelector('#createPlayerForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        createPlayer();
+    });
+
+    function createPlayer() {
+        var name = document.getElementById('name').value;
+    var position = document.getElementById('position').value;
+    var height = document.getElementById('height').value;
+    var nationality = document.getElementById('nationality').value;
+    var weight = document.getElementById('weight').value;
+
+    // Create player object
+    var newPlayer = {
+        "name": name,
+        "position": position,
+        "height": `${height}cm`,
+        "nationality": nationality,
+        "weight": `${weight}kg`,
+        "user": "6569631e738444d9987e170c"  // You may need to dynamically set the user ID
+    };
+
+    // Make AJAX request
+    fetch('http://127.0.0.1:2222/api/player/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPlayer),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            window.location.reload()
+        })
+        .then(data => {
+            // Handle the response data as needed
+            console.log(data);
+
+            // Close the modal
+            $('#createPlayerModal').modal('hide');
+        })
+        .catch(error => console.error('Error:', error));
+    }
+});
+
+
+
+
+
+
+// document.getElementById('createPlayerForm').addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     createPlayer();
+// });
+
+// function createPlayer() {
+//     // Get form values
+//     var name = document.getElementById('name').value;
+//     var position = document.getElementById('position').value;
+//     var height = document.getElementById('height').value;
+//     var nationality = document.getElementById('nationality').value;
+//     var weight = document.getElementById('weight').value;
+
+//     // Create player object
+//     var newPlayer = {
+//         "name": name,
+//         "position": position,
+//         "height": height,
+//         "nationality": nationality,
+//         "weight": weight,
+//         "user": "6569631e738444d9987e170c"  // You may need to dynamically set the user ID
+//     };
+
+//     // Make AJAX request
+//     fetch('http://127.0.0.1:2222/api/player/create', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(newPlayer),
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             window.location.reload()
+//         })
+//         .then(data => {
+//             // Handle the response data as needed
+//             console.log(data);
+
+//             // Close the modal
+//             $('#createPlayerModal').modal('hide');
+//         })
+//         .catch(error => console.error('Error:', error));
+
+// }
