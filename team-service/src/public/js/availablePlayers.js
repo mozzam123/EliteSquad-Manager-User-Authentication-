f59a4236df6c37cd83dc7933ab6aae9e0bda4c4f
mozzam123 <mozzam607@gmail.com>
2023-12-18 17:00:05 +0530

    added player add functionality

--- team-service/src/public/js/availablePlayers.js
+++ team-service/src/public/js/availablePlayers.js
@@ -1,10 +1,37 @@
-function clicked(name,height,nationality,weight) {
-    console.log("name", name);
-    console.log("height", height);
-    console.log("nationality", nationality);
-    console.log("weight", weight);
-   
+function clicked(name, height, nationality, weight) {
+    var name = name
+    var height = height
+    var nationality = nationality
+    var weight = weight
 
-    // Now you can use the 'height' variable in your logic.
-    // For example, you can update the 'name' retrieval as follows:
-}
+    // Create player object
+    var newPlayer = {
+        "name": name,
+        "position": "CF",
+        "height": `${height}cm`,
+        "nationality": nationality,
+        "weight": `${weight}kg`,
+        "user": "6569631e738444d9987e170c"  // You may need to dynamically set the user ID
+    };
+
+    // Make AJAX request
+    fetch('http://127.0.0.1:2222/api/player/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPlayer),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            window.location.reload()
+        })
+        .then(data => {
+            // Handle the response data as needed
+            console.log(data);
+
+        })
+        .catch(error => console.error('Error:', error));
+    }
