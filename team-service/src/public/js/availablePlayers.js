ab505f6e70c271217b24935d5034b36f01bd205b
mozzam123 <mozzam607@gmail.com>
2023-12-18 16:43:06 +0530

    fetched the api data from frontend

---
+++ team-service/src/public/js/availablePlayers.js
@@ -0,0 +1,10 @@
+function clicked(name,height,nationality,weight) {
+    console.log("name", name);
+    console.log("height", height);
+    console.log("nationality", nationality);
+    console.log("weight", weight);
+   
+
+    // Now you can use the 'height' variable in your logic.
+    // For example, you can update the 'name' retrieval as follows:
+}
