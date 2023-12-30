3c9b3185b6be07d748478641afa7e20fe69c979c
mozzam123 <mozzam607@gmail.com>
2023-12-30 16:28:28 +0530

    added correct error message on add player

--- team-service/src/public/js/availablePlayers.js
+++ team-service/src/public/js/availablePlayers.js
@@ -1,55 +1,54 @@
 function addPlayer(name, height, nationality, weight, amount) {
-    var name = name
-    var height = height
-    var nationality = nationality
-    var weight = weight
-    var amount = amount
+  var name = name;
+  var height = height;
+  var nationality = nationality;
+  var weight = weight;
+  var amount = amount;
 
-    // Create player object
-    var newPlayer = {
-        "name": name,
-        "position": "CF",
-        "height": height,
-        "nationality": nationality,
-        "weight": weight,
-        "user": "658bc1437b708a916818b14e",  // You may need to dynamically set the user ID
-        "amount": amount
-    };
-    console.log(newPlayer);
+  // Create player object
+  var newPlayer = {
+    name: name,
+    position: "CF",
+    height: height,
+    nationality: nationality,
+    weight: weight,
+    user: "658bc1437b708a916818b14e", // You may need to dynamically set the user ID
+    amount: amount,
+  };
+  console.log(newPlayer);
 
-    // Make AJAX request to create or add a new player
-    fetch('http://127.0.0.1:2222/api/player/create', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newPlayer),
+  // Make AJAX request to create or add a new player
+  fetch("http://127.0.0.1:2222/api/player/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPlayer),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((data) => {
+          showModal(data.message || "Cannot add more than 7 Players");
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        });
+      }
+      window.location.reload();
     })
-        .then(response => {
-            if (!response.ok) {
-                showModal("Cannot add more than 7 Players")
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            window.location.reload()
-            
-        })
-        .then(data => {
-            // Handle the response data as needed
-            console.log(data);
-
-        })
-        .catch(error => console.error('Error:', error));
+    .then((data) => {
+      // Handle the response data as needed
+      console.log(data);
+    })
+    .catch((error) => console.error("Error:", error));
 }
 
-
 function showModal(message) {
-    // Set the modal message
-    document.getElementById('modalMessage').innerText = message;
+  // Set the modal message
+  document.getElementById("modalMessage").innerText = message;
 
-    // Show the modal
-    $('#errorModal').modal('show');
+  // Show the modal
+  $("#errorModal").modal("show");
 }
 
 function closeModal() {
-    $('#errorModal').modal('hide');
+  $("#errorModal").modal("hide");
 }
