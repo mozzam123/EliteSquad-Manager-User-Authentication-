cd96edfaea470695771ba0391075f41238a2d69b
mozzam123 <mozzam607@gmail.com>
2024-01-01 15:15:56 +0530

    removed login and register api endpoint

--- auth-service/controllers/apiController.js
+++ auth-service/controllers/apiController.js
@@ -1,6 +1,5 @@
 const User = require("./../src/models/userModel")
 
-
 exports.loginUser = async (req, res) => {
     res.json({ message: "Hit login endpoint" });
 }
@@ -10,6 +9,7 @@ exports.createUser = async (req, res) => {
 }
 
 
+// Get All User
 exports.getAllUser = async (req, res) => {
     try {
         const allUsers = await User.find()
@@ -17,5 +17,19 @@ exports.getAllUser = async (req, res) => {
 
     } catch (error) {
         console.error("Error is: ", error)
+        // res.status(400).json({ status: "error", message: error })
     }
 }
+
+// Get User By ID
+// exports.getUser = async (req, res) => {
+//     try {
+//         const userInfo = await User.findById("658bc1437b708a916818b14e")
+//         res.status(200).json({ status: "success", result: userInfo })
+
+//     } catch (error) {
+//         console.error("Error is: ", error)
+//         res.status(400).json({ status: "error", message: error })
+
+//     }
+// }
