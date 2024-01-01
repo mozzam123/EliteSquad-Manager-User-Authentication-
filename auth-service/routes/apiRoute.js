b8f17a7ddfc9bbae8b853b995ff35178d86c847a
mozzam123 <mozzam607@gmail.com>
2024-01-01 15:16:00 +0530

    minor changes

--- auth-service/routes/apiRoute.js
+++ auth-service/routes/apiRoute.js
@@ -2,8 +2,6 @@ const express = require("express")
 const router = express.Router()
 const apiController = require("./../controllers/apiController")
 
-router.route('/create').get(apiController.createUser)
-router.route('/login').get(apiController.loginUser)
 router.route('/allusers').get(apiController.getAllUser)
 
 module.exports = router
