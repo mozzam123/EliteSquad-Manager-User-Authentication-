d4d0b8cb135a4dd1a76ffe408c6dbc13edf8903e
mozzam123 <mozzam607@gmail.com>
2024-01-25 16:57:48 +0530

    Added sendKafkaMessage in utils.js

---
+++ auth-service/config/kafkaConfig.js
@@ -0,0 +1,15 @@
+const { Kafka, logLevel } = require('kafkajs');
+
+// Create kafka producer instance
+const kafkaProducer = new Kafka({
+    brokers: ["localhost:9092"],
+    logLevel: logLevel.WARN
+}).producer()
+
+// Create kafka producer instance
+const kafkaConsumer = new Kafka({
+    brokers: ["localhost:9092"]
+}).consumer({ groupId: 'auth-service' })
+
+
+module.exports = { kafkaProducer, kafkaConsumer }
